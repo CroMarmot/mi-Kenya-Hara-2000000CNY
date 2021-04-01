e1239fff0cc4648b201e81a094cd4696c5a5fc7a
cromarmot <yexiaorain@gmail.com>
2021-04-01 19:41:49 +0800

    fix: reg $

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 export default function (source) {
   const lines = source.split(/\r\n|\r|\n/);
   lines.forEach((line,idx)=>{
-    if(/^\s*mi-Kenya-Hara\s*:\s*2000000CNY;?/i.test(line)){
-      lines[idx] = 'border-radius:19px';
+    if(/^\s*mi-Kenya-Hara\s*:\s*2000000CNY;?\s*$/i.test(line)){
+      lines[idx] = line.replace(/mi-Kenya-Hara\s*:\s*2000000CNY;?/,'border-radius:19px;');
     }
   })
   return lines.join('\r\n');
